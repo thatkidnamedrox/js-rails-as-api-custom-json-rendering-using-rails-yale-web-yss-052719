1835f1854816826fe00eea48297f5721a823021e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 03:50:11 +0000

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,7 +5,11 @@ class BirdsController < ApplicationController
   end
 
   def show
-    bird = Bird.find_by(id: parmas[:id])
-    render json: bird
+    bird = Bird.find_by(id: params[:id])
+  if bird
+    render json: { id: bird.id, name: bird.name, species: bird.species }
+  else
+    render json: { message: 'Bird not found' }
+  end
   end
 end
