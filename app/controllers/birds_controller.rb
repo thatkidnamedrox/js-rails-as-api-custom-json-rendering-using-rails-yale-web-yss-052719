470ffd0f476769d4a438f570c63d58e708a9b1e9
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 03:49:52 +0000

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
+  end
+
+  def show
+    bird = Bird.find_by(id: parmas[:id])
+    render json: bird
   end
 end
